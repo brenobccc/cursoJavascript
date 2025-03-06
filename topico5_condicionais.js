ad3a8ea8f78294dfcba2c96d9944385bc0c5a03c
brenobccc <brenobritobc@gmail.com>
2025-03-06 18:58:53 -0300

    novos topicos

---
+++ topico5_condicionais.js
@@ -0,0 +1,7 @@
+//verify se é maior de idade
+idade = 15
+if(idade >= 18){
+    console.log("Maiooorrr");
+}else{
+    console.log("Menorrr");
+}
---
+++ topico6_lacos.js
@@ -0,0 +1,4 @@
+//(inicialização, condicao, incremento ou decremento)
+for(let i = 0; i<=10; i++){
+    console.log("Ávila Mirley")
+}
